ab6f84065894ef1334a4a51a19d9701f0f0526e9
ynm3n <43065779+ynm3n@users.noreply.github.com>
2024-02-20 01:13:57 +0900

    main_test.go: rewrite test

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/ynm3n/go-cplib
 
 go 1.22.0
 
-require golang.org/x/exp v0.0.0-20240213143201-ec583247a57a
+require (
+	github.com/google/go-cmp v0.5.8
+	golang.org/x/exp v0.0.0-20240213143201-ec583247a57a
+)
--- main_test.go
+++ main_test.go
@@ -1,21 +1,83 @@
 package main
 
 import (
+	"bytes"
 	"io"
+	"os"
 	"testing"
+
+	gocmp "github.com/google/go-cmp/cmp"
 )
 
-func genRandomCase(t *testing.T, w io.WriteCloser) {
-	t.Helper()
-	defer w.Close()
-	// ここでテストケースを作る
+var (
+	testCount = 10000
+)
+
+func genTestCase(tb testing.TB, w io.Writer) {
+	tb.Helper()
+	// テストケースを作り、wに書き込むプログラムを書く
 }
 
-func TestSolve(t *testing.T) {
-	for range 10000 {
-		pr, pw := io.Pipe()
-		go genRandomCase(t, pw)
-		w := io.Discard // solveから結果を取得したい場合は変更する
-		Solve(pr, w)
+func genCorrect(tb testing.TB, r io.Reader, w io.Writer) {
+	tb.Helper()
+	// rからテストケースを取得し、正答をwに書き込むプログラムを書く
+}
+
+// 出力が正しいかどうか確認するためのテスト
+func TestSolve_Correct(t *testing.T) {
+	for range testCount {
+		sTestcase := new(bytes.Buffer)
+		cTestcase := new(bytes.Buffer)
+		mw := io.MultiWriter(sTestcase, cTestcase)
+		genTestCase(t, mw)
+
+		sAns := new(bytes.Buffer)
+		Solve(sTestcase, sAns)
+		cAns := new(bytes.Buffer)
+		genCorrect(t, cTestcase, cAns)
+
+		if d := gocmp.Diff(sAns.String(), cAns.String()); len(d) > 0 {
+			t.Fatal(d)
+		}
 	}
+}
+
+// panicするケースを探すためのテスト
+func TestSolve_Panic(t *testing.T) {
+	for range testCount {
+		buf := new(bytes.Buffer)
+		genTestCase(t, buf)
+		testcase := buf.String()
+
+		ok := t.Run(testcase, func(t *testing.T) {
+			defer func() {
+				if p := recover(); p != nil {
+					t.Fatal(p)
+				}
+				out(t, testcase)
+			}()
+			Solve(buf, io.Discard)
+		})
+		if !ok {
+			return
+		}
+	}
+}
+
+func BenchmarkSolve(b *testing.B) {
+	for i := 0; i < b.N; i++ {
+		buf := new(bytes.Buffer)
+		genTestCase(b, buf)
+		Solve(buf, io.Discard)
+	}
+}
+
+func out(tb testing.TB, testcase string) {
+	tb.Helper()
+	out, err := os.Create("out")
+	if err != nil {
+		tb.Fatalf("out(helper): %v", err)
+	}
+	defer out.Close()
+	out.WriteString(testcase)
 }
