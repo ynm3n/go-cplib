7c58f8b610cbc315328a817731789e51861f6ad9
ynm3n <43065779+ynm3n@users.noreply.github.com>
2024-02-19 18:45:23 +0900

    main.go: change template

--- main.go
+++ main.go
@@ -13,16 +13,17 @@ import (
 
 const (
 	bufferedOutput      = true
-	startScannerBufSize = 4096
+	initialInputBufSize = 1 << 15
 )
 
 // 解答欄
 func solve(in Input, out Output) {
+
 }
 
 // 入出力のバッファリング設定とsolve関数の呼び出し
 func Solve(r io.Reader, w io.Writer) {
-	in := NewInput(r)
+	in := NewInput(r, initialInputBufSize)
 	var out Output
 	if bufferedOutput {
 		bw := bufio.NewWriter(w)
@@ -41,29 +42,20 @@ func main() {
 	if os.Getenv("ATCODER") == "1" {
 		log.SetOutput(io.Discard)
 	} else {
-		flushLog := setLogBuffer()
-		defer flushLog()
+		const errHeader = "\n==========Stderr==========\n"
+		buf := bytes.NewBufferString(errHeader)
+		log.SetOutput(buf)
+		log.SetFlags(log.Lshortfile)
+		defer func() {
+			if buf.Len() > len(errHeader) {
+				io.Copy(os.Stderr, io.LimitReader(buf, 1<<10))
+				if buf.Len() > 0 {
+					fmt.Fprintln(os.Stderr, " …and more")
+				}
+			}
+		}()
 	}
 	Solve(os.Stdin, os.Stdout)
-}
-
-type Input interface {
-	ScanInt(a ...*int)
-	ScanFloat(a ...*float64)
-	ScanString(a ...*string)
-	ScanIntSlice(s []int)
-	ScanFloatSlice(s []float64)
-	ScanStringSlice(s []string)
-	Split(split bufio.SplitFunc)
-	Discard()
-}
-
-// default splitfunc: bufio.ScanWords
-func NewInput(r io.Reader) Input {
-	sc := bufio.NewScanner(r)
-	sc.Buffer(make([]byte, startScannerBufSize), math.MaxInt)
-	sc.Split(bufio.ScanWords)
-	return &input{sc}
 }
 
 type Output interface {
@@ -72,95 +64,115 @@ type Output interface {
 	Println(v ...any)
 }
 
+// default: not buffered
+// バッファリングしたい場合は *bufio.Writer を渡す
 func NewOutput(w io.Writer, prefix string, flag int) Output {
 	return log.New(w, prefix, flag)
 }
 
+type Input interface {
+	Split(split bufio.SplitFunc)
+	Discard()
+
+	Int() int
+	Int2() (int, int)
+	Int3() (int, int, int)
+	Int4() (int, int, int, int)
+	Float() float64
+	Float2() (float64, float64)
+	Float3() (float64, float64, float64)
+	Float4() (float64, float64, float64, float64)
+	String() string
+	String2() (string, string)
+	String3() (string, string, string)
+	String4() (string, string, string, string)
+	Runes() []rune
+
+	Ints(n int) []int
+	Floats(n int) []float64
+	Strings(n int) []string
+}
+
+// default splitfunc: bufio.ScanWords
+func NewInput(r io.Reader, bufSize int) Input {
+	sc := bufio.NewScanner(r)
+	sc.Buffer(make([]byte, bufSize), math.MaxInt)
+	sc.Split(bufio.ScanWords)
+	return &input{sc}
+}
+
+// Inputインターフェースの中身
 type input struct{ *bufio.Scanner }
 
-func (in *input) ScanInt(a ...*int) {
-	if err := scan(in, strconv.Atoi, a...); err != nil {
-		log.Panic(fmt.Errorf("ScanInt: %w", err))
+func (in *input) Split(split bufio.SplitFunc)                  { in.Scanner.Split(split) }
+func (in *input) Discard()                                     { in.Scan() }
+func (in *input) Int() int                                     { return in.i() }
+func (in *input) Int2() (int, int)                             { return in.i(), in.i() }
+func (in *input) Int3() (int, int, int)                        { return in.i(), in.i(), in.i() }
+func (in *input) Int4() (int, int, int, int)                   { return in.i(), in.i(), in.i(), in.i() }
+func (in *input) Float() float64                               { return in.f() }
+func (in *input) Float2() (float64, float64)                   { return in.f(), in.f() }
+func (in *input) Float3() (float64, float64, float64)          { return in.f(), in.f(), in.f() }
+func (in *input) Float4() (float64, float64, float64, float64) { return in.f(), in.f(), in.f(), in.f() }
+func (in *input) String() string                               { return in.s() }
+func (in *input) String2() (string, string)                    { return in.s(), in.s() }
+func (in *input) String3() (string, string, string)            { return in.s(), in.s(), in.s() }
+func (in *input) String4() (string, string, string, string)    { return in.s(), in.s(), in.s(), in.s() }
+func (in *input) Runes() []rune                                { return []rune(in.s()) }
+func (in *input) Ints(n int) []int {
+	res := make([]int, n)
+	for i := range res {
+		res[i] = in.i()
 	}
+	return res
+}
+func (in *input) Floats(n int) []float64 {
+	res := make([]float64, n)
+	for i := range res {
+		res[i] = in.f()
+	}
+	return res
+}
+func (in *input) Strings(n int) []string {
+	res := make([]string, n)
+	for i := range res {
+		res[i] = in.s()
+	}
+	return res
 }
 
-func (in *input) ScanFloat(a ...*float64) {
-	if err := scan(in, func(s string) (float64, error) { return strconv.ParseFloat(s, 64) }, a...); err != nil {
-		log.Panic(fmt.Errorf("ScanFloat: %w", err))
+func (in *input) i() int {
+	if err := in.checkScan(); err != nil {
+		log.Panicln(fmt.Errorf("input int: %w", err))
 	}
-}
-
-func (in *input) ScanString(a ...*string) {
-	if err := scan(in, func(s string) (string, error) { return s, nil }, a...); err != nil {
-		log.Panic(fmt.Errorf("ScanString: %w", err))
+	res, err := strconv.Atoi(in.Text())
+	if err != nil {
+		log.Panicln(fmt.Errorf("input int: %w", err))
 	}
+	return res
 }
-
-func (in *input) ScanIntSlice(s []int) {
-	if err := scanSlice(in, strconv.Atoi, s); err != nil {
-		log.Panic(fmt.Errorf("ScanIntSlice: %w", err))
+func (in *input) f() float64 {
+	if err := in.checkScan(); err != nil {
+		log.Panicln(fmt.Errorf("input float: %w", err))
 	}
-}
-
-func (in *input) ScanFloatSlice(s []float64) {
-	if err := scanSlice(in, func(s string) (float64, error) { return strconv.ParseFloat(s, 64) }, s); err != nil {
-		log.Panic(fmt.Errorf("ScanFloatSlice: %w", err))
+	res, err := strconv.ParseFloat(in.Text(), 64)
+	if err != nil {
+		log.Panicln(fmt.Errorf("input float: %w", err))
 	}
+	return res
 }
-
-func (in *input) ScanStringSlice(s []string) {
-	if err := scanSlice(in, func(s string) (string, error) { return s, nil }, s); err != nil {
-		log.Panic(fmt.Errorf("ScanStringSlice: %w", err))
+func (in *input) s() string {
+	if err := in.checkScan(); err != nil {
+		log.Panicln(fmt.Errorf("input string: %w", err))
 	}
+	return in.Text()
 }
-
-func (in *input) Split(split bufio.SplitFunc) { in.Scanner.Split(split) }
-
-func (in *input) Discard() { in.Scan() }
-
-func scan[T any](in *input, f func(s string) (T, error), a ...*T) error {
-	var err error
-	for _, p := range a {
-		if !in.Scan() {
-			if err = in.Err(); err != nil {
-				return err
-			}
-			return io.EOF
-		}
-		if *p, err = f(in.Text()); err != nil {
+func (in *input) checkScan() error {
+	if !in.Scan() {
+		if err := in.Err(); err != nil {
 			return err
 		}
+		return io.EOF
 	}
 	return nil
-}
-
-func scanSlice[T any](in *input, f func(s string) (T, error), s []T) error {
-	var err error
-	for i := range s {
-		if !in.Scan() {
-			if err = in.Err(); err != nil {
-				return err
-			}
-			return io.EOF
-		}
-		if s[i], err = f(in.Text()); err != nil {
-			return err
-		}
-	}
-	return nil
-}
-
-func setLogBuffer() func() {
-	const errHeader = "\n==========Stderr==========\n"
-	buf := bytes.NewBufferString(errHeader)
-	log.SetOutput(buf)
-	log.SetFlags(log.Lshortfile)
-	return func() {
-		if buf.Len() > len(errHeader) {
-			io.Copy(os.Stderr, io.LimitReader(buf, 1<<10))
-			if buf.Len() > 0 {
-				fmt.Fprintln(os.Stderr, " …and more")
-			}
-		}
-	}
 }
