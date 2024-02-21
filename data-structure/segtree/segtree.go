e8550240b79e5f2a7080143fbee05b40d56da875
ynm3n <43065779+ynm3n@users.noreply.github.com>
2024-02-21 17:50:43 +0900

    data-structure/segtree: refactor

--- data-structure/segtree/segtree.go
+++ data-structure/segtree/segtree.go
@@ -8,18 +8,34 @@ type SegmentTree[T any] interface {
 	ProductAll() T
 }
 
-func NewSegmentTree[T any](s []T, op func(a, b T) T, unit T) SegmentTree[T] {
+func NewSegmentTree[T any](n int, e func() T, op func(a, b T) T) SegmentTree[T] {
+	data := make([]T, n*2)
+	for i := range data {
+		data[i] = e()
+	}
 	sg := &segmentTree[T]{
-		n:         len(s),
-		data:      make([]T, len(s)*2),
-		operation: op,
-		unit:      unit,
+		n:    n,
+		data: data,
+		e:    e,
+		op:   op,
 	}
-	for i := 0; i < sg.n; i++ {
-		sg.data[i] = sg.unit
+	for i := sg.n - 1; i >= 1; i-- {
+		sg.update(i)
 	}
-	for i, v := range s {
-		sg.data[i+sg.n] = v
+	return sg
+}
+
+func NewSegmentTreeWith[T any](s []T, e func() T, op func(a, b T) T) SegmentTree[T] {
+	data := make([]T, len(s)*2)
+	for i := 0; i < len(s); i++ {
+		data[i] = e()
+	}
+	copy(data[len(s):], s)
+	sg := &segmentTree[T]{
+		n:    len(s),
+		data: data,
+		e:    e,
+		op:   op,
 	}
 	for i := sg.n - 1; i >= 1; i-- {
 		sg.update(i)
@@ -33,46 +49,46 @@ func NewSegmentTree[T any](s []T, op func(a, b T) T, unit T) SegmentTree[T] {
 // https://github.com/monkukui/ac-library-go/blob/master/segtree/segtree.go
 // https://atcoder.github.io/ac-library/master/document_ja/segtree.html
 type segmentTree[T any] struct {
-	n         int            // 初期化時に渡したスライスの要素数
-	data      []T            // セグメントツリーの本体
-	operation func(a, b T) T // モノイド積(?)を計算する関数
-	unit      T              // 単位元
+	n    int            // 初期化時に渡したスライスの要素数
+	data []T            // セグメントツリーの本体
+	e    func() T       // 単位元
+	op   func(a, b T) T // モノイド積(?)を計算する関数
 }
 
 func (sg *segmentTree[T]) Len() int {
 	return sg.n
 }
 
-func (sg *segmentTree[T]) Set(idx int, v T) {
-	now := sg.n + idx
-	sg.data[now] = v
+func (sg *segmentTree[T]) Set(i int, val T) {
+	now := sg.n + i
+	sg.data[now] = val
 	for now > 1 {
 		now /= 2
 		sg.update(now)
 	}
 }
 
-func (sg *segmentTree[T]) Get(idx int) T {
-	return sg.data[sg.n+idx]
+func (sg *segmentTree[T]) Get(i int) T {
+	return sg.data[sg.n+i]
 }
 
 func (sg *segmentTree[T]) Product(l, r int) T {
 	l += sg.n
 	r += sg.n
-	valL, valR := sg.unit, sg.unit
+	valL, valR := sg.e(), sg.e()
 	for l < r {
 		if l%2 == 1 {
-			valL = sg.operation(valL, sg.data[l])
+			valL = sg.op(valL, sg.data[l])
 			l++
 		}
 		if r%2 == 1 {
 			r--
-			valR = sg.operation(sg.data[r], valR)
+			valR = sg.op(sg.data[r], valR)
 		}
 		l /= 2
 		r /= 2
 	}
-	return sg.operation(valL, valR)
+	return sg.op(valL, valR)
 }
 
 func (sg *segmentTree[T]) ProductAll() T {
@@ -81,5 +97,5 @@ func (sg *segmentTree[T]) ProductAll() T {
 
 func (sg *segmentTree[T]) update(now int) {
 	child1, child2 := now*2, now*2+1
-	sg.data[now] = sg.operation(sg.data[child1], sg.data[child2])
+	sg.data[now] = sg.op(sg.data[child1], sg.data[child2])
 }
