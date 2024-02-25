2f33da522850c22565b4448e30e3a062cbf7f5e5
ynm3n <43065779+ynm3n@users.noreply.github.com>
2024-02-25 18:14:41 +0900

    data-structure/priorityqueue: compare -> less
    等しいかどうかの情報は不要なためlessで比較する

--- data-structure/priorityqueue/priorityqueue.go
+++ data-structure/priorityqueue/priorityqueue.go
@@ -14,20 +14,22 @@ type PriorityQueue[T any] interface {
 
 // 昇順
 func NewPriorityQueue[T cmp.Ordered]() PriorityQueue[T] {
-	pq := NewPriorityQueueFunc(cmp.Compare[T])
+	pq := NewPriorityQueueFunc(cmp.Less[T])
 	return pq
 }
 
-func NewPriorityQueueFunc[T any](compare func(a, b T) int) PriorityQueue[T] {
+// lessは並んでる時にtrue、並んでない時にfalseを返すような関数
+// 参考: https://pkg.go.dev/cmp@go1.22.0#Less
+func NewPriorityQueueFunc[T any](less func(a, b T) bool) PriorityQueue[T] {
 	pq := new(priorityQueue[T])
 	pq.s = make([]T, 0)
-	pq.compare = compare
+	pq.less = less
 	return pq
 }
 
 type priorityQueue[T any] struct {
-	s       []T
-	compare func(a, b T) int
+	s    []T
+	less func(a, b T) bool
 }
 
 func (pq *priorityQueue[T]) Top() T      { return pq.s[0] }
@@ -36,7 +38,7 @@ func (pq *priorityQueue[T]) Dequeue() T  { return heap.Pop(pq).(T) }
 
 func (pq priorityQueue[T]) Len() int           { return len(pq.s) }
 func (pq priorityQueue[T]) Swap(i, j int)      { pq.s[i], pq.s[j] = pq.s[j], pq.s[i] }
-func (pq priorityQueue[T]) Less(i, j int) bool { return pq.compare(pq.s[i], pq.s[j]) < 0 }
+func (pq priorityQueue[T]) Less(i, j int) bool { return pq.less(pq.s[i], pq.s[j]) }
 func (pq *priorityQueue[T]) Push(x any) {
 	pq.s = append(pq.s, x.(T))
 }
