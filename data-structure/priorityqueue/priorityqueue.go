aada1281713ba1df3ff0f53b73541dc72bf4d339
ynm3n <43065779+ynm3n@users.noreply.github.com>
2024-02-25 18:04:28 +0900

    data-structure/priorityqueue: add interface

--- data-structure/priorityqueue/priorityqueue.go
+++ data-structure/priorityqueue/priorityqueue.go
@@ -5,42 +5,44 @@ import (
 	"container/heap"
 )
 
-type PriorityQueue[T any] struct {
-	h *hp[T]
+type PriorityQueue[T any] interface {
+	Len() int
+	Top() T
+	Enqueue(x T)
+	Dequeue() T
 }
 
 // 昇順
-func NewPriorityQueue[T cmp.Ordered]() *PriorityQueue[T] {
+func NewPriorityQueue[T cmp.Ordered]() PriorityQueue[T] {
 	pq := NewPriorityQueueFunc(cmp.Compare[T])
 	return pq
 }
 
-func NewPriorityQueueFunc[T any](compare func(a, b T) int) *PriorityQueue[T] {
-	pq := new(PriorityQueue[T])
-	pq.h.s = make([]T, 0)
-	pq.h.compare = compare
+func NewPriorityQueueFunc[T any](compare func(a, b T) int) PriorityQueue[T] {
+	pq := new(priorityQueue[T])
+	pq.s = make([]T, 0)
+	pq.compare = compare
 	return pq
 }
 
-func (pq *PriorityQueue[T]) Len() int    { return pq.h.Len() }
-func (pq *PriorityQueue[T]) Top() T      { return pq.h.s[0] }
-func (pq *PriorityQueue[T]) Enqueue(x T) { heap.Push(pq.h, x) }
-func (pq *PriorityQueue[T]) Dequeue() T  { return heap.Pop(pq.h).(T) }
-
-type hp[T any] struct {
+type priorityQueue[T any] struct {
 	s       []T
 	compare func(a, b T) int
 }
 
-func (h hp[T]) Len() int           { return len(h.s) }
-func (h hp[T]) Swap(i, j int)      { h.s[i], h.s[j] = h.s[j], h.s[i] }
-func (h hp[T]) Less(i, j int) bool { return h.compare(h.s[i], h.s[j]) < 0 }
-func (h *hp[T]) Push(x any) {
-	h.s = append(h.s, x.(T))
+func (pq *priorityQueue[T]) Top() T      { return pq.s[0] }
+func (pq *priorityQueue[T]) Enqueue(x T) { heap.Push(pq, x) }
+func (pq *priorityQueue[T]) Dequeue() T  { return heap.Pop(pq).(T) }
+
+func (pq priorityQueue[T]) Len() int           { return len(pq.s) }
+func (pq priorityQueue[T]) Swap(i, j int)      { pq.s[i], pq.s[j] = pq.s[j], pq.s[i] }
+func (pq priorityQueue[T]) Less(i, j int) bool { return pq.compare(pq.s[i], pq.s[j]) < 0 }
+func (pq *priorityQueue[T]) Push(x any) {
+	pq.s = append(pq.s, x.(T))
 }
-func (h *hp[T]) Pop() any {
-	n := len(h.s)
-	res := (h.s)[n-1]
-	h.s = (h.s)[:n-1]
+func (pq *priorityQueue[T]) Pop() any {
+	n := len(pq.s)
+	res := (pq.s)[n-1]
+	pq.s = (pq.s)[:n-1]
 	return res
 }
